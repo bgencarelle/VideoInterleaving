9fcfcc8ff7ccc49f70439697b8e28a54bc618a74
bgencarelle <bgencarelle@gmail.com>
2023-03-23 18:18:22 +0100

    almost working.
    Keeps skipping
    Code by ChatGPT4

--- Main.py
+++ Main.py
@@ -1,34 +1,51 @@
 import os
 import glob
 import cv2
+import numpy as np
 
+# Disable libpng warnings
+os.environ['PNG_WARNINGS'] = '0'
+
+# Read folder locations and store them in a list
 with open('folder_locations.txt', 'r') as f:
     folders = f.read().splitlines()
-png_sequences = {}
-frame_rate = 30  # Change this to your desired frame rate
 
-# Scan each folder for PNG files
-for folder in folders:
-    png_files = glob.glob(os.path.join(folder, '*.png'))
-    png_sequences[folder] = sorted(png_files, key=lambda x: int(os.path.splitext(os.path.basename(x))[0]))  # Fix indentation here
+# Read frames order and store them in a list
+with open('frames_order.txt', 'r') as f:
+    frame_list = f.read().splitlines()
 
-# Merge sequences
-merged_sequence = []
-max_frames = max([len(seq) for seq in png_sequences.values()])
+# Get user inputs for the number of frames to process and black frames between each frame
+max_frames = int(input("Enter the number of frames to process: "))
+black_frames_between = int(input("Enter the number of black frames between each frame: "))
 
-for i in range(max_frames):
-    for folder in folders:
-        if i < len(png_sequences[folder]):
-            merged_sequence.append(png_sequences[folder][i])
+# Read the first frame to get dimensions for the video
+first_frame = cv2.imread(frame_list[0], cv2.IMREAD_UNCHANGED)
+height, width, _ = first_frame.shape
 
-# Create the video
-output_video = 'output_video.avi'
-frame = cv2.imread(merged_sequence[0])
-height, width, _ = frame.shape
-video = cv2.VideoWriter(output_video, cv2.VideoWriter_fourcc(*'MJPG'), frame_rate, (width, height))
+# Create the video writer
+output_video = 'output_video.mp4'
+video = cv2.VideoWriter(output_video, cv2.VideoWriter_fourcc(*'avc1'), 30, (width, height), isColor=True)
 
-for png in merged_sequence:
-    frame = cv2.imread(png)
-    video.write(frame)
+# Process the frames and create the video
+for index, png in enumerate(frame_list[:max_frames]):
+    print(f"Processing frame {index + 1}/{max_frames}")
 
+    # Read the frame with transparency
+    frame = cv2.imread(png, cv2.IMREAD_UNCHANGED)
+
+    # Convert the frame to BGRA and then to BGR
+    bgra = cv2.cvtColor(frame, cv2.COLOR_BGRA2RGBA)
+    bgr_bg = cv2.cvtColor(bgra, cv2.COLOR_RGBA2BGR)
+
+    # Replace transparent pixels with green
+    bgr_bg[bgra[:, :, 3] == 0] = [0, 255, 0]
+
+    # Write the frame to the video
+    video.write(bgr_bg)
+
+    # Add black frames
+    for _ in range(black_frames_between):
+        video.write(np.zeros((height, width, 3), dtype=np.uint8))
+
+# Release the video writer
 video.release()
