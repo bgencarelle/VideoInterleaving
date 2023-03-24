5a8f4b35191ef19c98bc5fd12b952b9040db8ac7
Ben Gencarelle <bgencarelle@gmail.com>
2023-03-24 13:17:21 +0100

    Working-ish

--- videomerge.py
+++ videomerge.py
@@ -6,8 +6,18 @@ with open("sorted.txt", "r") as file:
     png_files = [line.strip() for line in file]
 
 # Prompt the user for the number of frames and the frames per second
-num_frames = int(input("Enter the number of frames for the video: "))
-fps = int(input("Enter the frames per second: "))
+print("the default length is " +str(len(png_files)) +" frames long")
+num_frames_input = input("Enter the number of frames for the video: ")
+if num_frames_input.isdigit() and int(num_frames_input) < len(png_files):
+    num_frames = int(num_frames_input)
+else:
+    num_frames = len(png_files)
+
+fps_input = input("Enter the frames per second (default is 30): ")
+if fps_input.isdigit():
+    fps = int(fps_input)
+else:
+    fps = 30  # default to 30 FPS
 
 # Calculate and display the video length
 video_length = num_frames / fps
@@ -23,14 +33,21 @@ out = cv2.VideoWriter("output_video.mp4", fourcc, fps, (width, height))
 for frame_number in range(num_frames):
     # Determine which PNG to use (repeat list if necessary)
     png_index = frame_number % len(png_files)
-
+    print("frame number:" +str(png_index))
+    print(png_files[png_index])
     # Read the PNG image and resize if needed
     image = cv2.imread(png_files[png_index], cv2.IMREAD_UNCHANGED)
     if image.shape[:2] != (height, width):
         image = cv2.resize(image, (width, height))
 
+    # Check if the image has an alpha channel; if not, create one with full opacity
+    if image.shape[2] == 3:
+        alpha_channel = np.full((height, width), 255, dtype=np.uint8)
+        image = cv2.merge((image, alpha_channel))
+    else:
+        alpha_channel = image[:, :, 3]
+
     bgr_image = image[:, :, :3]
-    alpha_channel = image[:, :, 3]
 
     # Create a green background with the same dimensions as the input images
     green_bg = np.zeros((height, width, 3), dtype=np.uint8)
