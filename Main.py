dca119c8ea6d4449b80cd14c80f1b9bea78c483a
bgencarelle <bgencarelle@gmail.com>
2023-05-03 15:02:49 +0200

    found fullscreen glitch

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import os
 from get_folders_list import write_folder_list
 from make_png_lists import process_files
-from midi_mtc_old import mtc_png_realtime
 from image_display import display_and_run
 
 def main():
