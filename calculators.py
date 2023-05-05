6d76bd94ee5abf1c4c6a4eb288bf0d495c2b21de
bgencarelle <bgencarelle@gmail.com>
2023-05-06 00:25:26 +0200

    mtc is working! Now to tweak it a bit for smoother playback.

---
+++ calculators.py
@@ -0,0 +1,134 @@
+import csv
+import os
+import mido
+
+png_paths_len = 1
+frame_duration = 1.0
+video_length = 1
+
+
+def get_midi_length(midi_file_path):
+    midi_file = mido.MidiFile(midi_file_path)
+    midi_length_seconds = midi_file.length
+    frames_per_second = 30  # Assuming 30 FPS
+    midi_length_frames = int(midi_length_seconds * frames_per_second)
+    return midi_length_frames
+
+
+def calculate_frame_duration(text_mode=True):
+    global png_paths_len, frame_duration, video_length
+    if text_mode:
+        video_length = int(input("Please specify the length of the video in frames: "))
+        if video_length <= 30:
+            raise ValueError("Length should be greater than 30.")
+    else:
+        midi_file_path = input("Please enter the MIDI file path: ")
+        if not os.path.exists(midi_file_path):
+            raise FileNotFoundError("MIDI file not found.")
+        if not midi_file_path.lower().endswith('.mid'):
+            raise ValueError("Invalid file type. Please provide a valid MIDI file.")
+        video_length = get_midi_length(midi_file_path)
+
+    frame_duration = video_length / png_paths_len
+    print("Frame scaling factor for this video: ", frame_duration)
+    return frame_duration
+
+
+def select_csv_file():
+    csv_dir = 'generatedPngLists'
+    csv_files = [f for f in os.listdir(csv_dir) if f.endswith('.csv')]
+
+    if not csv_files:
+        print(f"No .csv files found in directory {csv_dir}")
+        return None
+
+    if len(csv_files) == 1:
+        print("Only one .csv file found, defaulting to:")
+        selected_file = os.path.join(csv_dir, csv_files[0])
+        print(selected_file)
+        return selected_file
+
+    print("Please select a .csv file to use:")
+    for i, f in enumerate(csv_files):
+        print(f"{i + 1}: {f}")
+
+    while True:
+        try:
+            selection = int(input("> "))
+            if selection not in range(1, len(csv_files) + 1):
+                raise ValueError
+            break
+        except ValueError:
+            print("Invalid selection. Please enter a number corresponding to a file.")
+
+    selected_file = os.path.join(csv_dir, csv_files[selection - 1])
+    print(f"Selected file: {selected_file}")
+    return selected_file
+
+
+def get_image_names_from_csv(file_path):
+    global png_paths_len
+    with open(file_path, 'r') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        png_paths = [row[1:] for row in csv_reader]
+        png_paths_len = len(png_paths)
+    return png_paths
+
+
+def check_index_differences():
+    previous_index = None
+    result_counts = {}
+    video_length_mx = video_length
+
+    for i in range(video_length_mx):
+        current_index, _ = calculate_index(i*2)
+
+        # Count the unique results
+        if current_index not in result_counts:
+            result_counts[current_index] = 0
+        result_counts[current_index] += 1
+
+        # Check index differences
+        if previous_index is not None and abs(current_index - previous_index) > 1:
+            print(f"Index difference greater than 1 found between steps {i - 1} and {i}")
+
+        previous_index = current_index
+
+    print("Number of times each unique result appears:")
+    for result, count in result_counts.items():
+        print(f"Result: {result}, Count: {count}")
+
+
+def calculate_index(estimate_frame_counter, index_mult=4.0):
+    global frame_duration, png_paths_len
+    progress = (estimate_frame_counter / (frame_duration / index_mult)) % (png_paths_len * 2)
+
+    if int(progress) <= png_paths_len:
+        index = int(progress)
+        direction = 1
+    else:
+        index = int(png_paths_len * 2 - progress)
+        direction = -1
+
+    # Ensure the index is within the valid range
+    index = max(0, min(index, png_paths_len - 1))
+    print(index, direction)
+
+    return index, direction
+
+
+
+def init_all():
+    csv_source = select_csv_file()
+    png_paths = get_image_names_from_csv(csv_source)
+    # print(png_paths_len == len(png_paths), png_paths_len)
+    frame_dur = calculate_frame_duration()
+
+
+def main():
+    init_all()
+    # print(frame_dur)
+    check_index_differences()
+
+if __name__ == "__main__":
+    main()
--- midi_control.py
+++ midi_control.py
@@ -1,7 +1,17 @@
 import mido
-from concurrent.futures import ThreadPoolExecutor
-import threading
-import sys
+import calculators
+
+
+frame_duration = 1
+video_len = 1
+source_frame_total = 1
+mtc_received = False
+recent_messages = []
+total_frames = 0
+frame_rate = 30
+mtc_values = [0, 0, 0, 0]
+midi_port = None
+index = 0
 
 
 def select_midi_input():
@@ -43,105 +53,146 @@ def select_midi_input():
         except ValueError:
             print("Invalid selection. Please enter a number corresponding to a port.")
 
+def process_midi():
+    with mido.open_input(midi_port) as in_port:
+        for msg in in_port:
+            if msg.type == 'quarter_frame':
+                process_mtc(msg)
+            elif msg.type in ('note_on', 'note_off', 'mod_wheel', 'program_change', 'clock'):
+                process_message(msg)
 
-def check_input_key():
-    while True:
-        key = input()
-        if key == 'q':
-            sys.exit()
+
+def process_mtc(msg):
+    global mtc_received, recent_messages, total_frames, frame_rate, mtc_values
+
+    mtc_type, value = parse_mtc(msg)
+    update_mtc_timecode(mtc_type, value)
+    recent_messages.append(msg)
+
+    if is_complete_mtc_code():
+        mtc_received = True
+        calculate_frame_rate()
+        calculate_time_code()
+        calculate_total_frames()
+        mtc_received = False
+        recent_messages.clear()
 
 
-class MidiProcessor:
-    def __init__(self):
-        self.mtc_received = threading.Event()
-        self.recent_messages = []
-        self.total_frames = 0
-        self.frame_rate = None
-        self.mtc_values = [0, 0, 0, 0]
 
-    def process_midi(self, port):
-        with mido.open_input(port) as in_port:
-            for msg in in_port:
-                if msg.type == 'quarter_frame':
-                    self.process_mtc(msg)
-                elif msg.type in ('note_on', 'note_off', 'mod_wheel', 'program_change', 'clock'):
-                    self.process_message(msg)
+def is_complete_mtc_code():
+    # Assuming all 8 messages have been received when the list has 8 elements
+    return len(recent_messages) == 8
 
-    def process_mtc(self, msg):
-        mtc_type, value = self.parse_mtc(msg)
-        self.update_mtc_timecode(mtc_type, value)
-        self.recent_messages.append(msg)
 
-        if self.is_complete_mtc_code():
-            self.mtc_received.set()
-            self.calculate_frame_rate()
-            self.calculate_time_code()
-            self.calculate_total_frames()
-            self.mtc_received.clear()
-            self.recent_messages.clear()
+def calculate_frame_rate():
+    global frame_rate
+    # Calculate the frame rate
+    # Assuming 30 fps for now, you can update this as needed
+    frame_rate = 30
 
-    def process_message(self, msg):
-        pass
-        # Process non-MTC messages as needed
 
-    def is_complete_mtc_code(self):
-        # Assuming all 8 messages have been received when the list has 8 elements
-        return len(self.recent_messages) == 8
+def calculate_time_code():
+    hours = mtc_values[0] & 0x1F
+    minutes = mtc_values[1]
+    seconds = mtc_values[2]
+    frames = mtc_values[3]
+    time_code = f'{hours:02}:{minutes:02}:{seconds:02}:{frames:02}'
+    print(f"Time code: {time_code}")
 
-    def calculate_frame_rate(self):
-        # Calculate the frame rate
-        # Assuming 30 fps for now, you can update this as needed
-        self.frame_rate = 30
+def parse_mtc(msg):
+    mtc_type = msg.frame_type
+    value = msg.frame_value
+    return mtc_type, value
 
-    def calculate_time_code(self):
-        hours = self.mtc_values[0] & 0x1F
-        minutes = self.mtc_values[1]
-        seconds = self.mtc_values[2]
-        frames = self.mtc_values[3]
-        time_code = f'{hours:02}:{minutes:02}:{seconds:02}:{frames:02}'
-        print(f"Time code: {time_code}")
 
-    def parse_mtc(self, msg):
-        mtc_type = msg.frame_type
-        value = msg.frame_value
-        return mtc_type, value
+def update_mtc_timecode(mtc_type, value):
+    global mtc_values
 
-    def update_mtc_timecode(self, mtc_type, value):
-        if mtc_type == 0:
-            self.mtc_values[3] = (self.mtc_values[3] & 0xF0) | value
-        elif mtc_type == 1:
-            self.mtc_values[3] = (self.mtc_values[3] & 0x0F) | (value << 4)
-        elif mtc_type == 2:
-            self.mtc_values[2] = (self.mtc_values[2] & 0xF0) | value
-        elif mtc_type == 3:
-            self.mtc_values[2] = (self.mtc_values[2] & 0x0F) | (value << 4)
-        elif mtc_type == 4:
-            self.mtc_values[1] = (self.mtc_values[1] & 0xF0) | value
-        elif mtc_type == 5:
-            self.mtc_values[1] = (self.mtc_values[1] & 0x0F) | (value << 4)
-        elif mtc_type == 6:
-            self.mtc_values[0] = (self.mtc_values[0] & 0xF0) | value
-        elif mtc_type == 7:
-            self.mtc_values[0] = (self.mtc_values[0] & 0x0F) | (value << 4)
+    if mtc_type == 0:
+        mtc_values[3] = (mtc_values[3] & 0xF0) | value
+    elif mtc_type == 1:
+        mtc_values[3] = (mtc_values[3] & 0x0F) | (value << 4)
+    elif mtc_type == 2:
+        mtc_values[2] = (mtc_values[2] & 0xF0) | value
+    elif mtc_type == 3:
+        mtc_values[2] = (mtc_values[2] & 0x0F) | (value << 4)
+    elif mtc_type == 4:
+        mtc_values[1] = (mtc_values[1] & 0xF0) | value
+    elif mtc_type == 5:
+        mtc_values[1] = (mtc_values[1] & 0x0F) | (value << 4)
+    elif mtc_type == 6:
+        mtc_values[0] = (mtc_values[0] & 0xF0) | value
+    elif mtc_type == 7:
+        mtc_values[0] = (mtc_values[0] & 0x0F) | (value << 4)
 
-    def calculate_total_frames(self):
-        hours = self.mtc_values[0] & 0x1F
-        minutes = self.mtc_values[1]
-        seconds = self.mtc_values[2]
-        frames = self.mtc_values[3]
-        total_frames = (hours * 60 * 60 * self.frame_rate) + \
-                       (minutes * 60 * self.frame_rate) + (seconds * self.frame_rate) + frames
-        self.total_frames = total_frames
 
-        print(f"Total frames: {self.total_frames}")
-        return self.total_frames
+def calculate_total_frames():
+    global total_frames, frame_rate, mtc_values
 
-def midi_control_stuff():
-    midi_processor = MidiProcessor()
+    hours = mtc_values[0] & 0x1F
+    minutes = mtc_values[1]
+    seconds = mtc_values[2]
+    frames = mtc_values[3]
+    total_frames = (hours * 60 * 60 * frame_rate) + \
+                   (minutes * 60 * frame_rate) + (seconds * frame_rate) + frames
+    total_frames = total_frames
+
+    #print(f"Total frames: {total_frames}")
+    return total_frames
+
+
+
+
+def handle_note_on(msg):
+    print(f"Note on: {msg.note}, velocity: {msg.velocity}, channel: {msg.channel}")
+
+
+def handle_note_off(msg):
+    print(f"Note off: {msg.note}, velocity: {msg.velocity}, channel: {msg.channel}")
+
+
+def handle_mod_wheel(msg):
+    print(f"Modulation wheel: value: {msg.value}, channel: {msg.channel}")
+
+
+def handle_program_change(msg):
+    print(f"Program change: program: {msg.program}, channel: {msg.channel}")
+
+
+def handle_clock(msg):
+    clock_counter(1)
+    # print("Clock message received")
+
+def clock_counter(amount=None):
+    if not hasattr(clock_counter, "counter"):
+        clock_counter.counter = 0
+
+    if amount is not None:
+        clock_counter.counter += amount
+
+    return clock_counter.counter
+
+def process_message(msg):
+    handler = message_handlers.get(msg.type)
+    if handler:
+        handler(msg)
+    else:
+        print(f"Unhandled message type: {msg.type}")
+
+
+
+
+message_handlers = {
+    'note_on': handle_note_on,
+    'note_off': handle_note_off,
+    'mod_wheel': handle_mod_wheel,
+    'program_change': handle_program_change,
+    'clock': handle_clock,
+}
+def midi_control_stuff_main():
     midi_port = select_midi_input()
-    midi_processor.process_midi(midi_port)
-    return midi_processor
+    calculators.init_all()
+    #process_midi()
 
 if __name__ == "__main__":
-    midi_control_stuff()
-
+    midi_control_stuff_main()
