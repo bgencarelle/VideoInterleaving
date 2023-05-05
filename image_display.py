c541e91f46d431a01d80da82b974cfa9b862070d
bgencarelle <bgencarelle@gmail.com>
2023-05-06 00:25:26 +0200

    mtc is working! Now to tweak it a bit for smoother playback.

--- image_display.py
+++ image_display.py
@@ -1,27 +1,26 @@
-import csv
-import os
 import sys
 import time
 from concurrent.futures import ThreadPoolExecutor
 from queue import SimpleQueue
 
+import calculators
+from calculators import select_csv_file, get_image_names_from_csv
+import midi_control
 import cv2
 import pygame.time
 from OpenGL.GL import *
 from OpenGL.GLU import *
-import platform
-from midi_control import MidiProcessor
-from midi_control import select_midi_input
+from platform import system
+import threading
 
-if platform.system() == 'Darwin':
+if system() == 'Darwin':
     from Quartz import CGWindowListCopyWindowInfo, kCGWindowListOptionOnScreenOnly, kCGNullWindowID
-elif platform.system() == 'Linux':
+elif system() == 'Linux':
     from Xlib import X, display
 
 import platform
 import pygame
 from pygame.locals import *
-
 
 def is_window_maximized():
     if platform.system() == 'Darwin':
@@ -53,63 +52,24 @@ def is_window_maximized():
         raise NotImplementedError(f"Maximized window detection is not implemented for {platform.system()}")
 
 
-def select_csv_file():
-    csv_dir = 'generatedPngLists'
-    csv_files = [f for f in os.listdir(csv_dir) if f.endswith('.csv')]
-
-    if len(csv_files) == 0:
-        print(f"No .csv files found in directory {csv_dir}")
-        return None
-
-    if len(csv_files) == 1:
-        print("Only one .csv file found, defaulting to:")
-        selected_file = os.path.join(csv_dir, csv_files[0])
-        print(selected_file)
-        return selected_file
-
-    print("Please select a .csv file to use:")
-    for i, f in enumerate(csv_files):
-        print(f"{i + 1}: {f}")
-
-    while True:
-        try:
-            selection = int(input("> "))
-            if selection not in range(1, len(csv_files) + 1):
-                raise ValueError
-            break
-        except ValueError:
-            print("Invalid selection. Please enter a number corresponding to a file.")
-
-    selected_file = os.path.join(csv_dir, csv_files[selection - 1])
-    print(f"Selected file: {selected_file}")
-    return selected_file
-
-
-def get_aspect_ratio(image_path):
-    image = cv2.imread(image_path)
-    h, w, _ = image.shape
-    aspect_ratio = h / w
-    return aspect_ratio, w
-
-
-def get_image_names_from_csv(file_path):
-    with open(file_path, 'r') as csvfile:
-        csv_reader = csv.reader(csvfile)
-        png_paths = [row[1:] for row in csv_reader]
-
-    return png_paths
-
-
-def event_check(image_size, fullscreen, index, text_mode=False):
+def event_check(image_size, fullscreen, index, direction = 1, text_mode=False, force_quit=False):
     width, height = image_size
     aspect_ratio = width / height
+    old_direction = 1
+    paused = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
             sys.exit()
+            pygame.quit()
         if event.type == KEYDOWN:
+            if event.key == K_q:  # Press 'p' key to toggle pause
+                force_quit=True
+                print("bailing out")
+                pygame.quit()
+                sys.exit()
             if event.key == K_f:  # Press 'f' key to toggle fullscreen
-                fullscreen = toggle_fullscreen(image_size, fullscreen)
+                pass
+               # fullscreen = toggle_fullscreen(image_size, fullscreen)
             if event.key == K_i:  # Press 'i' key to jump around
                 index += 500
                 print("jump around")
@@ -124,7 +84,19 @@ def event_check(image_size, fullscreen, index, text_mode=False):
                 new_height = int(new_width / aspect_ratio)
             display_init((new_width, new_height), window_check)
 
-    return fullscreen, index, text_mode
+    return fullscreen, index, text_mode, force_quit
+
+def get_aspect_ratio(image_path):
+    # Load image with Pygame
+    image = pygame.image.load(image_path)
+
+    # Get image dimensions
+    w, h = image.get_size()
+
+    # Calculate aspect ratio
+    aspect_ratio = h / w
+
+    return aspect_ratio, w
 
 
 def toggle_fullscreen(image_size, current_fullscreen_status):
@@ -194,18 +166,20 @@ def load_images(index, png_paths, main_folder, float_folder):
     return main_image, float_image
 
 
-def run_display(index, png_paths, main_folder, float_folder, image_size):
+def run_display(index, direction, png_paths, main_folder, float_folder, image_size):
     font_size = 24
     font = pygame.font.Font(None, font_size)
     text_display = False
     fps = 30
     clock = pygame.time.Clock()
-    fullscreen = True
-    print_update = False
-
+    fullscreen = False
+    print_update = True
+    force_quit = False
+    midi_mode = True
     current_time = time.time()
     prev_time = current_time
     old_index = index
+
 
     # Initialize PINGPONG related variables
     PINGPONG = True
@@ -215,6 +189,10 @@ def run_display(index, png_paths, main_folder, float_folder, image_size):
     main_image, float_image = load_images(index, png_paths, main_folder, float_folder)
     texture_id1 = create_texture(main_image)
     texture_id2 = create_texture(float_image)
+    def queue_image(index, direction, png_paths, main_folder, float_folder, image_queue):
+        index = max(0, min(index, len(png_paths) - 1))
+        image_future = executor.submit(load_images, index, png_paths, main_folder, float_folder)
+        image_queue.put(image_future)
 
     with ThreadPoolExecutor(max_workers=4) as executor:
         image_queue = SimpleQueue()
@@ -224,38 +202,38 @@ def run_display(index, png_paths, main_folder, float_folder, image_size):
         for _ in range(buffer_size):
             index += direction
             index = max(0, min(index, len(png_paths) - 1))
-            image_future = executor.submit(load_images, index, png_paths, main_folder, float_folder)
-            image_queue.put(image_future)
+            queue_image(index, direction, png_paths, main_folder, float_folder, image_queue)
 
-        while True:
+        while not force_quit:
 
-            fullscreen, index, text_display = event_check(image_size, fullscreen, index, text_display)
+
+            fullscreen, index, text_display, force_quit = event_check(image_size, fullscreen, index, text_display, force_quit)
             if not image_queue.empty():
                 main_image, float_image = image_queue.get().result()
                 load_texture(texture_id1, main_image)
                 load_texture(texture_id2, float_image)
-
-                if PINGPONG:
-                    # Reverse direction at boundaries
-                    if (index + direction) < 0 or index >= len(png_paths) - 1:
-                        direction = -direction
-
-                index = (index + direction) % (len(png_paths) - 1)
+                if midi_mode:
+                    total_frames = midi_control.total_frames
+                    index, direction = calculators.calculate_index(total_frames)
+                if not midi_mode:
+                    if PINGPONG:
+                        # Reverse direction at boundaries
+                        if (index + direction) < 0 or index >= len(png_paths) - 1:
+                            direction = -direction
+                        index = (index + direction) % (len(png_paths) - 1)
 
                 current_time = time.time()
-                if (current_time - prev_time >= 1) and print_update:
+                if (current_time - prev_time >= 4) and print_update:
                     print("index: ", index, "   index diff:  ", index - old_index)
                     old_index = index
                     prev_time = current_time
-                    float_folder = (float_folder + 1) % (len(png_paths[0]) - 1)
+                    float_folder = (float_folder + 1) % (len(png_paths[0]) )
                     part = clock.get_fps()
                     print(part)
 
                 # Start a new future for the next images
                 next_index = index + buffer_size * direction
-                next_index = max(0, min(next_index, len(png_paths) - 1))
-                image_future = executor.submit(load_images, next_index, png_paths, main_folder, float_folder)
-                image_queue.put(image_future)
+                queue_image(next_index, direction, png_paths, main_folder, float_folder, image_queue)
 
             overlay_images_fast(texture_id1, texture_id2, image_size)
 
@@ -269,7 +247,8 @@ def setup_blending():
 
 
 def display_init(image_size, fullscreen=False, setup=False):
-    pygame.init()
+    if setup:
+        pygame.init()
     width, height = image_size
     fullscreen_size = pygame.display.list_modes()[0]
     fullscreen_width, fullscreen_height = fullscreen_size
@@ -284,16 +263,17 @@ def display_init(image_size, fullscreen=False, setup=False):
         flags |= FULLSCREEN
         pygame.display.set_caption('Fullscreen Mode')
         pygame.display.set_mode((fullscreen_width, fullscreen_height), flags)
-
         glViewport(offset_x, offset_y, int(width * scale), int(height * scale))
     else:
         flags |= RESIZABLE
-        pygame.display.set_mode(image_size, flags)
-        pygame.display.set_caption('Windowed Mode')
-        print(pygame.display.list_modes())
-        glViewport(0, 0, width, height)
+
+        if pygame.display.mode_ok(image_size, flags):
+            pygame.display.set_mode(image_size, flags)
+            pygame.display.set_caption('Windowed Mode')
+        #print(pygame.display.list_modes())
+            glViewport(0, 0, width, height)
         # Explicitly set the window size back to image_size when returning to windowed mode
-        pygame.display.set_mode(image_size, flags)
+            pygame.display.set_mode(image_size, flags)
 
     if setup:
         glEnable(GL_TEXTURE_2D)
@@ -305,24 +285,28 @@ def display_init(image_size, fullscreen=False, setup=False):
         glLoadIdentity()
 
 
+
 def display_and_run():
-    midi_port = select_midi_input()
-    midi_stuff = MidiProcessor()
-    # midi_stuff.process_midi(midi_port)
+    midi_control.midi_control_stuff_main()
     print(platform.system())
     csv_source = select_csv_file()
     png_paths = get_image_names_from_csv(csv_source)
     print(len(png_paths[0]))
     aspect_ratio, width = get_aspect_ratio(png_paths[0][0])
     print(width)
-    width = 640
     height = int(width * aspect_ratio)
     image_size = (width, height)
-    start_index = 0
     main_folder = 6
-    float_folder = 6
-    display_init(image_size, True, True)
-    run_display(start_index, png_paths, main_folder, float_folder, image_size)
+    float_folder = 0
+    fullscreen = False
+    setup_mode = True
+    threading.Thread(target=midi_control.process_midi).start()
+    start_frames = midi_control.total_frames
+    print(start_frames)
+    index,direction = calculators.calculate_index(start_frames)
+    display_init(image_size, fullscreen, setup_mode)
+    run_display(index, direction,png_paths, main_folder, float_folder, image_size)
+
 
 
 if __name__ == "__main__":
