8ab937cba988ed03d52247b669f595a10ef50a8a
bgencarelle <bgencarelle@gmail.com>
2023-05-14 01:37:27 +0200

    Integrated midi clock and mtc. Now there are 4 modes of operation.

--- midi_control.py
+++ midi_control.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from collections import deque
 
@@ -5,29 +6,34 @@ import mido
 
 import calculators
 
-
-CLOCK_BUFFER_SIZE = 200
+MTC_CLOCK = 0
+MIDI_CLOCK = 1
+MIXED_CLOCK = 2
+CLOCK_MODE = 2
+CLOCK_BUFFER_SIZE = 50
 TIMEOUT_SECONDS = 1  # Set the timeout value as needed
 
+###globals, sorry not sorry
+clock_mode = CLOCK_MODE  # 0 is mtc, 1 is midi_clock, 2 is hybrid (tempo from midi, rough location from mtc)
+png_paths_len = 0
 clock_counter_sum = 0
-clock_mode = 1  # 0 is mtc, 1 is midi_clock
 frame_duration = 1
 video_len = 1
 source_frame_total = 1
+clock_frame_ratio = 1
 mtc_received = False
 recent_messages = []
 mtc_values = [0, 0, 0, 0]
 input_port = None
 index = 0
 frame_rate = 30
-direction = 1
-clock_index = 0
-clock_index_direction = 1
+index_direction = 1
 last_received_mtc_time = 0  # Initialize last received MTC message time
 total_frames = 0
 frame_type_count = {i: 0 for i in range(8)}  # Initialize frame type count
 new_mtc_code_started = [False]
 bpm = 120
+clock_frame_ratio = 1.0
 
 
 def select_midi_input():
@@ -70,48 +76,54 @@ def select_midi_input():
             print("Invalid selection. Please enter a number corresponding to a port.")
 
 
-def process_midi(mode=clock_mode):
+def process_midi(mode=CLOCK_MODE):
     global clock_mode
     clock_mode = mode
     in_port = input_port
     for msg in in_port.iter_pending():
-        if msg.type == 'quarter_frame':
+        if msg.type == 'quarter_frame':  # and clock_mode == 0:
             process_mtc(msg)
-        elif msg.type in ('note_on', 'note_off', 'mod_wheel', 'program_change', 'clock'):
+        elif msg.type in (
+                'note_on',
+                'note_off',
+                'mod_wheel',
+                'program_change',
+                'clock',
+                'stop',
+                'start',
+                'continue',
+                'songpos',
+                'reset',
+        ):
             process_message(msg)
     return
 
 
 def process_mtc(msg):
-    global mtc_received, frame_rate, mtc_values, index, direction, total_frames, clock_counter_sum
+    global mtc_received, frame_rate, mtc_values, index, index_direction, total_frames, clock_counter_sum, clock_frame_ratio
     mtc_type, value = parse_mtc(msg)
     update_mtc_timecode(mtc_type, value)
 
     if mtc_type == 7:  # Recalculate timecode once FRAMES LSB quarter-frame is received
-        old_total_frames = total_frames
-        old_total_clocks = clock_counter_sum
         mtc_received = True
+        old_total_frames = total_frames
         calculate_time_code()
         total_frames = calculate_total_frames()
-        frames_by_2 = total_frames // 2
-        clock_counter_sum = clock_counter()
-        clock_diff = (clock_counter_sum - old_total_clocks) * 24
-        total_frame_diff = abs(total_frames - old_total_frames)
-        #print("clock_diff", clock_diff)
-        if total_frame_diff > 2:
+        if total_frames <= 0:
+            clock_counter(0)
+        total_frame_diff = abs(total_frames-old_total_frames)
+        if total_frame_diff > 4:
             print(f'difference: {total_frame_diff},at frame: {total_frames}')
-        # print(bpm)
-        index, direction = calculators.calculate_index(frames_by_2 + clock_diff)
-        # print(bpm, "INDEX:", index*direction, "FRAMES:", total_frames)
-
+            clock_counter(0)
+            clock_counter(clock_frame_ratio * total_frames)
+        if clock_mode == MTC_CLOCK:
+            index, index_direction = calculators.calculate_index(total_frames)
+        clock_counter_sum = clock_counter()
+        if total_frames > 2:
+            clock_frame_ratio = clock_counter_sum / total_frames
+        #print("clock counts since 0: ", clock_counter_sum, "clock_frame_ratio * frames: ", ratio_frames)
+        #print("bpm: ", bpm, "INDEX: ", index*index_direction)
         mtc_received = False
-
-
-def update_mtc_timecode(mtc_type, value):
-    global mtc_values
-
-    # Update the buffer with the received quarter-frame value
-    mtc_values[mtc_type] = value
 
 
 def calculate_time_code():
@@ -169,6 +181,13 @@ def calculate_total_frames():
     return calc_frames
 
 
+def handle_stop(msg):
+    global total_frames
+    clock_counter(0)
+    total_frames = 0
+    print(f"STOP")
+
+
 def handle_note_on(msg):
     print(f"Note on: {msg.note}, velocity: {msg.velocity}, channel: {msg.channel}")
 
@@ -186,18 +205,22 @@ def handle_program_change(msg):
 
 
 def handle_clock(msg):
-    global clock_index, clock_index_direction, bpm
+    global index, index_direction, bpm, total_frames
 
     if not hasattr(handle_clock, "clock_intervals"):
         handle_clock.clock_intervals = deque(maxlen=CLOCK_BUFFER_SIZE)
         handle_clock.intervals_sum = 0
         handle_clock.last_clock_time = None
+        handle_clock.last_total_frames = None
+        handle_clock.last_total_frames_time = None
 
     current_time = time.time()
 
     # Check if the timeout has been exceeded
     if handle_clock.last_clock_time is not None and current_time - handle_clock.last_clock_time > TIMEOUT_SECONDS:
-        print("Clock messages stopped. No BPM update.")
+        clock_counter(0)
+        total_frames = 0
+        print("Clock messages stopped.")
     else:
         if handle_clock.last_clock_time is not None:
             clock_interval = current_time - handle_clock.last_clock_time
@@ -215,10 +238,53 @@ def handle_clock(msg):
                 # print(f"BPM: {bpm:.2f}")
 
     handle_clock.last_clock_time = current_time
+
+    # Check if total_frames has not changed in 200ms
+    if clock_mode != MIDI_CLOCK:
+        if handle_clock.last_total_frames is not None and handle_clock.last_total_frames == total_frames\
+                and current_time - handle_clock.last_total_frames_time > 0.2:
+            clock_counter(0)
+            print("Total frames haven't changed in 200ms. Clock reset to zero.")
+        else:
+            handle_clock.last_total_frames = total_frames
+            handle_clock.last_total_frames_time = current_time
+
     # Your original code
     clock_counter(1)
-    if clock_mode == 1:
-        clock_index, clock_index_direction = calculators.calculate_index(clock_counter(), False)
+    if clock_mode == MIDI_CLOCK:
+        index, index_direction = calculators.calculate_index(clock_counter())
+    elif clock_mode == MIXED_CLOCK:
+        index, index_direction = calculators.calculate_index(clock_counter() * clock_frame_ratio)
+    print(f'clock counter: {clock_counter()}, Index: {index * index_direction}')
+
+
+def handle_start(msg):
+    global total_frames
+    clock_counter(0)
+    total_frames = 0
+    print("Start message received.")
+    # Add your handling code for the Start message here
+
+
+def handle_continue(msg):
+    global total_frames
+    clock_counter(0)
+    total_frames = 0
+    print("Continue message received.", total_frames)
+    # Add your handling code for the Continue message here
+
+
+def handle_song_position_pointer(msg):
+    global total_frames
+    clock_counter(0)
+    total_frames = 0
+    print(f"Song Position Pointer message received. Value: {msg.pos}")
+    # Add your handling code for the Song Position Pointer message here
+
+
+def handle_system_reset(msg):
+    print("System Reset message received.")
+    # Add your handling code for the System Reset message here
 
 
 def clock_counter(amount=None):
@@ -226,8 +292,11 @@ def clock_counter(amount=None):
         clock_counter.counter = 0
 
     if amount is not None:
-        clock_counter.counter += amount
-    # print(clock_counter.counter)
+        if amount == 0 or total_frames <= 1 and clock_mode != MIDI_CLOCK:
+            clock_counter.counter = 0
+        else:
+            clock_counter.counter += int(amount)
+    # print("clock_counter.counter says: ", clock_counter.counter)
     return clock_counter.counter
 
 
@@ -245,18 +314,24 @@ message_handlers = {
     'mod_wheel': handle_mod_wheel,
     'program_change': handle_program_change,
     'clock': handle_clock,
+    'stop': handle_stop,
+    'start': handle_start,
+    'continue': handle_continue,
+    'songpos': handle_song_position_pointer,
+    'system_reset': handle_system_reset,
 }
 
 
 def midi_control_stuff_main():
-    global input_port
+    global input_port, png_paths_len
+    _, png_paths = calculators.init_all()
+    png_paths_len = len(png_paths)
     midi_port = select_midi_input()
     input_port = mido.open_input(midi_port)
 
 
 def main():
     midi_control_stuff_main()
-    calculators.init_all()
     while True:
         process_midi()
 
