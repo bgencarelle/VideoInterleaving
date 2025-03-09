bc88e42ade4256d2cc7f4314cb41cdb5e4c893e6
Ben Gencarelle <bgencarelle@gmail.com>
2025-03-09 15:09:25 +0100

    adding stuffs

--- settings.py
+++ settings.py
@@ -1,14 +1,14 @@
 # settings.py
 # Display Mode
 
-FULLSCREEN_MODE = True
+FULLSCREEN_MODE = False
 ##
 IPS = 30
 FPS = 60
 
 BUFFER_SIZE = IPS // 4  # e.g., 15 if IPS==60
 PINGPONG = True
-
+FROM_BIRTH = True
 # Clock Mode Constants
 MTC_CLOCK = 0
 MIDI_CLOCK = 1
@@ -16,6 +16,8 @@ MIXED_CLOCK = 2
 CLIENT_MODE = 3
 FREE_CLOCK = 255
 CLOCK_MODE = FREE_CLOCK
+
+MIDI_MODE = False
 
 # Valid Clock Modes (for interactive selection)
 VALID_MODES = {
