d0ec24bb81eff3d8b8925a0c857aaae86d265f49
Ben Gencarelle <bgencarelle@gmail.com>
2025-03-09 13:05:40 +0100

    Oh wow. Found a thing I had missed for years-was updating every frame instead of everytime the index changed.

--- settings.py
+++ settings.py
@@ -2,10 +2,11 @@
 # Display Mode
 
 FULLSCREEN_MODE = True
+##
 IPS = 30
 FPS = 60
 
-BUFFER_SIZE = IPS // 4  # e.g., 15 if FPS==60
+BUFFER_SIZE = IPS // 4  # e.g., 15 if IPS==60
 PINGPONG = True
 
 # Clock Mode Constants
