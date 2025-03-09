16d9d85a3918585466b9abbbfd53fc8359991026
Ben Gencarelle <bgencarelle@gmail.com>
2025-03-09 12:29:26 +0100

    Oh.

---
+++ globals.py
@@ -0,0 +1,12 @@
+# globals.py
+control_data_dictionary = {
+    'Note_On': (0, 127, 0),
+    'Note_Off': (None, None, None),
+    'Modulation': (0, 0),
+    'Index_and_Direction': (0, 1),
+    'BPM': 120,
+}
+
+folder_dictionary = {
+    'Main_and_Float_Folders': (0, 0),
+}
