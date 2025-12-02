039ac0264666ff133103ddbf9647651754c61e3b
Ben Gencarelle <bgencarelle@gmail.com>
2025-12-02 23:24:07 +0100

    trying to optimize

--- settings.py
+++ settings.py
@@ -162,5 +162,5 @@ STREAM_PORT = 8080           # Port for video stream (distinct from monitor port
 STREAM_HOST = '0.0.0.0'      # Listen on all interfaces
 SERVER_CAPTURE_RATE = 25     # Streaming FPS
 JPEG_QUALITY = 85         # Image quality
-HEADLESS_RES = (640, 640)   # Resolution for the virtual screen
+HEADLESS_RES = (512, 640)   # Resolution for the virtual screen
 HEADLESS_BACKEND = "egl"      # "egl" or "osmesa"
