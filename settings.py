e26ae0924d75f09195c098f0617bd718f7506333
Ben Gencarelle <bgencarelle@gmail.com>
2025-12-02 21:22:33 +0100

    deleted shit branch

--- settings.py
+++ settings.py
@@ -161,7 +161,7 @@ HEADLESS_USE_GL = True    # new: disable ModernGL headless on VPS
 STREAM_PORT = 8080           # Port for video stream (distinct from monitor port 1978)
 STREAM_HOST = '0.0.0.0'      # Listen on all interfaces
 SERVER_CAPTURE_RATE = 30     # Streaming FPS
-JPEG_QUALITY = 95         # Image quality
+JPEG_QUALITY = 85         # Image quality
 HEADLESS_RES = (640, 640)   # Resolution for the virtual screen
 HEADLESS_BACKEND = "egl"      # "egl" or "osmesa"
 # Streaming / JPEG controls
