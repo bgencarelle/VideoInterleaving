e5aee87296df05fabc68744e5ffda67352110c79
Ben Gencarelle <bgencarelle@gmail.com>
2025-12-10 02:21:44 +0100

    setting this up to be more network friendly.

--- constantStorage/server_constants.py
+++ constantStorage/server_constants.py
@@ -2,9 +2,9 @@
 import sys
 
 STREAM_PORT = 8080           # Port for video stream (distinct from monitor port 1978)
-STREAM_HOST = '127.0.0.1'      # Listen on all interfaces
+STREAM_HOST = '0.0.0.0'      # Listen on all interfaces
 JPEG_QUALITY = 75# Image quality
-HEADLESS_RES = (400, 533)   # Resolution for the virtual screen
+HEADLESS_RES = (600, 800)   # Resolution for the virtual screen
 
 if sys.platform == "darwin":
     HEADLESS_BACKEND = None
