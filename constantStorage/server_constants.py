13a9c3132a47050acac7ba909204c593461726ae
Ben Gencarelle <bgencarelle@gmail.com>
2025-12-16 00:55:03 +0100

    Setting up something a bit different.
    Trying a new website.

--- constantStorage/server_constants.py
+++ constantStorage/server_constants.py
@@ -2,7 +2,7 @@
 import sys
 WEBSOCKET_PORT = 2324
 STREAM_PORT = 8080           # Port for video stream (distinct from monitor port 1978)
-STREAM_HOST = '127.0.0.1'      # Listen on all interfaces
+STREAM_HOST = '0.0.0.0'      # Listen on all interfaces
 
 if sys.platform == "darwin":
     HEADLESS_BACKEND = None
